71d3626cccae6d333af97c6e252c73bd623b4e8b
raulmtsdev <raulmts.dev@gmail.com>
2024-04-23 21:25:02 -0300

    PROJETO C#

---
+++ Projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double x = 10.35784;
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
