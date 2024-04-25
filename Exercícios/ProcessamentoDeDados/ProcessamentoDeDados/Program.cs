5962c958953f99f0b1eca61fffb6c8cb9ec73336
raulmtsdev <raulmts.dev@gmail.com>
2024-04-24 21:10:58 -0300

    Adicionar pasta exercicios

---
+++ Exercícios/ProcessamentoDeDados/ProcessamentoDeDados/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProcessamentoDeDados_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine(); 
+            string z = Console.ReadLine();
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+           
+     
+            Console.WriteLine(" ");
+            Console.WriteLine("Resultado");
+            Console.WriteLine(" ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+
+            
+
+
+        }
+    }
+}
